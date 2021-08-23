bcf0239f7e4e1c256244e8edd2b7671a082760d2
Neha <goyalneha2607@gmail.com>
2021-08-23 18:20:58 +0530

    db added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const session = require("express-session");
 const passport = require("passport");
 const expressSanitizer = require("express-sanitizer");
 const LocalStrategy = require("passport-local");
+const astra = require("./models/astra");
+
 require('dotenv').config();
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,59 +20,71 @@ app.use(session({
 	saveUninitialized:false
 }));
 
-const createClient  = require("@astrajs/collections");
-const collection = "user"
 
-// create an {astra_db} client
-exports.handler = async function (event, context, callback) {
-const astraClient =  await createClient({
-    astraDatabaseId: process.env.ASTRA_DB_ID,
-    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-    applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
-});
-// const astraClient =  createClient({
-//     astraDatabaseId: process.env.ASTRA_DB_ID,
-//     astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-//     username: process.env.ASTRA_DB_USERNAME,
-//     password: process.env.ASTRA_DB_PASSWORD,
-//   });
+// const createClient  = require("@astrajs/collections");
+// const collection = "user"
 
-var User =  astraClient
-    .namespace(process.env.ASTRA_DB_KEYSPACE)
-    .collection(collection);
-}
-    // app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStrategy(
-//     function(username, password, done) {
-//       User.findOne({ username: username }, function (err, user) {
-//         if (err) { return done(err); }
-//         if (!user) { return done(null, false); }
-//         if (!user.verifyPassword(password)) { return done(null, false); }
-//         return done(null, user);
-//       });
-//     }
-//   ));
-  
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-// }
-app.get("/", function(req, res){
-  try {
-    console.log("hello");
-		const user =  await User.create("Hello",{name: "whatsup"})
+// // create an {astra_db} client
+// exports.handler = async function (event, context, callback) {
+// 	astraClient =  await createClient({
+// 		astraDatabaseId: process.env.ASTRA_DB_ID,
+// 		astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+// 		applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+// 	});
+// // const astraClient =  createClient({
+// //     astraDatabaseId: process.env.ASTRA_DB_ID,
+// //     astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+// //     username: process.env.ASTRA_DB_USERNAME,
+// //     password: process.env.ASTRA_DB_PASSWORD,
+// //   });
     
-		return {
-		  statusCode: 200,
-		  body: JSON.stringify(user),
-		};
-	  } catch (e) {
-		console.error(e);
-		return {
-		  statusCode: 500,
-		  body: JSON.stringify(e),
-		}
-	}  
+// }
+// var User =  astraClient
+//     	.namespace(process.env.ASTRA_DB_KEYSPACE)
+//     	.collection(collection);
+//     // app.use(passport.initialize());
+// // app.use(passport.session());
+// // passport.use(new LocalStrategy(
+// //     function(username, password, done) {
+// //       User.findOne({ username: username }, function (err, user) {
+// //         if (err) { return done(err); }
+// //         if (!user) { return done(null, false); }
+// //         if (!user.verifyPassword(password)) { return done(null, false); }
+// //         return done(null, user);
+// //       });
+// //     }
+// //   ));
+  
+// // passport.serializeUser(User.serializeUser());
+// // passport.deserializeUser(User.deserializeUser());
+// // }
+app.get("/", async function(req, res){
+//   try {
+// 	const userClient = await getAstraClient();
+// 	const userCollection =  userClient
+// 	  .namespace(process.env.ASTRA_DB_KEYSPACE)
+// 	  .collection("colors");
+
+    // console.log("hello");
+	// 	const user = userCollection.create("Hello",{name: "whatsup"})
+    
+	// 	return {
+	// 	  statusCode: 200,
+	// 	  body: JSON.stringify(user),
+	// 	};
+	//   } catch (e) {
+	// 	console.error(e);
+	// 	return {
+	// 	  statusCode: 500,
+	// 	  body: JSON.stringify(e),
+	// 	}
+	// }  
+	await astra.addColorHistory({
+		name: "yellow",
+		value: 1,
+	  });
+	const sample = await astra.getColorHistory();
+	console.log(sample);
   res.send("Hi");
 })
 app.listen(3000, process.env.IP, function(){
