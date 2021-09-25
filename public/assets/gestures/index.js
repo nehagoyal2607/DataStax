aa2604c1ad8ffb652638898365ac5feb1adfcecd
Neha <goyalneha2607@gmail.com>
2021-09-25 16:18:48 +0530

    added gesture detection

--- public/assets/gestures/index.js
+++ public/assets/gestures/index.js
@@ -1,9 +1,11 @@
 import VictoryGesture from './Victory';
 import ThumbsUpGesture from './ThumbsUp';
 import LoveYouGesture from './LoveYou';
+import AGesture from './aletter';
 
 export {
   VictoryGesture,
   ThumbsUpGesture,
-  LoveYouGesture
+  LoveYouGesture,
+  AGesture
 }
